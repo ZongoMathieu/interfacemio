efa864ab8495d7224f0a0d284843757357b4eff9
unknown <holding@CHO-65.coris-bank.com>
2020-10-26 14:43:55 +0000

    Ajout de telechargement fichier excel dans le component visiteur

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -12,6 +12,34 @@ export class ServicesComponent implements OnInit {
   public visitees;
   public serviceSelectionned={id:-1};
   public message;
+  public data;
+  dataExcel: any = [
+    {
+       
+        "CATEGORYID": 1,
+        "CATEGORYNAME": "BOOKS",
+        "DESCRIPTION": "It contains all types of books",
+        "IMAGE": "Books",
+        "STATUS": "TRUE"
+    },
+    {
+       
+        "CATEGORYID": 2,
+        "CATEGORYNAME": "EBOOKS",
+        "DESCRIPTION": "It contains all types of ebooks",
+        "IMAGE": "Ebooks",
+        "STATUS": "TRUE"
+    },
+    {
+     
+        "CATEGORYID": 3,
+        "CATEGORYNAME": "Bookss",
+        "DESCRIPTION": "DESCRIPTION",
+        "IMAGE": "IMAGE",
+        "STATUS": "TRUE"
+    }
+];
+
   constructor(private appServ:AppService) { }
 
   ngOnInit(): void {
@@ -43,5 +71,23 @@ export class ServicesComponent implements OnInit {
       console.log(err);
     })
   }
+
+  onSave(data){
+    console.log(data);
+    this.appServ.getResource("/saveServices/");
+  }
+
+  //Funtion to export excel file
+  exportAsXLSX():void {
+    this.appServ.getResource("/services/")
+    .subscribe(data=>{
+      this.data=data;
+      console.log(data);
+      this.appServ.exportAsExcelFile(this.data, 'myExcelFile');
+    },err=>{
+      console.log(err);
+    })
+   
+ }
 
 }
--- src/app/visiteurs/visiteurs.component.ts
+++ src/app/visiteurs/visiteurs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../appservice.service';
+import * as XLSX from 'xlsx'; 
 
 @Component({
   selector: 'app-visiteurs',
@@ -8,6 +9,8 @@ import { AppService } from '../appservice.service';
 })
 export class VisiteursComponent implements OnInit {
   public visiteurs;
+  fileName= '';
+  todayDate : Date = new Date();
 
   constructor(private appSer: AppService) { }
 
@@ -25,5 +28,28 @@ export class VisiteursComponent implements OnInit {
       console.log(err);
     })
   }
+
+  exportexcel(): void 
+    {
+      let annee =this.todayDate.getFullYear();
+      console.log(annee);
+      this.fileName="FGD_CBI_31-12"+annee+"_S_DEF_1_EXL.EXL.xlsx";
+       /* table id is passed over here */  
+     
+       let element = document.getElementById('excel-table'); 
+       const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+       const ws2: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+
+       /* generate workbook and add the worksheet */
+       const wb: XLSX.WorkBook = XLSX.utils.book_new();
+       XLSX.utils.book_append_sheet(wb, ws, '31-12-'+(annee-1));
+       XLSX.utils.book_append_sheet(wb, ws2, '31-12-AVEC'+(annee-1));
+
+       console.log(this.todayDate);
+       /* save to file */
+       XLSX.writeFile(wb, this.fileName);
+      
+       
+    }
 
 }
